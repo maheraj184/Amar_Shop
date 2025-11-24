718fda271e4cb878570edc198975b9cf6908874b
Maheraj Hossen <110416768+maheraj184@users.noreply.github.com>
2025-11-24 19:45:13 +0600

    Update urls.py

--- EcomProject/urls.py
+++ EcomProject/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
 
 handler404 = "django.views.defaults.page_not_found"
 handler500 = "django.views.defaults.server_error"
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

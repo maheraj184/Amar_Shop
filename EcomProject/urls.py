777e0428cccd3f3f33ef70babb4a0a78629798e5
Maheraj Hossen <110416768+maheraj184@users.noreply.github.com>
2025-11-24 19:40:21 +0600

    Update urls.py

--- EcomProject/urls.py
+++ EcomProject/urls.py
@@ -7,10 +7,12 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("", include("home.urls")),
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),  # shop app er URLs
+    path('shop', include('shop.urls')),  # shop app er URLs
     path('users/', include('users.urls')),
     path('contact/', include('contact.urls')),
 ]
 
 handler404 = "django.views.defaults.page_not_found"
 handler500 = "django.views.defaults.server_error"
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
